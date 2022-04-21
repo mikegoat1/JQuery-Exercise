51b137cc99e151e6c713abf81d2aacd5554ec4a3
Mikey J <mikeylashaun@icloud.com>
2022-04-20 18:32:18 -0700

    worked on moment function/setInterval Fucntion

--- Main/Asset/script.js
+++ Main/Asset/script.js
@@ -1,12 +1,13 @@
 // Create a variable that connects to the hero class in the header. Or that connects to the dateTime class
 let dateTime = $(".dateTime"); 
+console.log(dateTime); 
 // Set up a setinterval() fuction so you can update the text in the hero. 
     // Every second update the format of moment with a new version of info
 
 
 function setTime() {
     let clock = setInterval(function() {
-        dateTime.textContent = moment().format("dddd, MMM Do YYY, h:mm:ss a"); 
+        dateTime.text(moment().format("MMMM Do YYYY, h:mm:ss a")) 
     }, 1000); 
 };
 
