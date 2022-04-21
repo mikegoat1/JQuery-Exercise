553b3cb7b5992827de6ee75d75b42c530f03351c
Mikey J <mikeylashaun@icloud.com>
2022-04-20 18:19:08 -0700

    changes I made at work. Added some functions to script

---
+++ Main/Asset/script.js
@@ -0,0 +1,13 @@
+// Create a variable that connects to the hero class in the header. Or that connects to the dateTime class
+let dateTime = $(".dateTime"); 
+// Set up a setinterval() fuction so you can update the text in the hero. 
+    // Every second update the format of moment with a new version of info
+
+
+function setTime() {
+    let clock = setInterval(function() {
+        dateTime.textContent = moment().format("dddd, MMM Do YYY, h:mm:ss a"); 
+    }, 1000); 
+};
+
+setTime(); 
