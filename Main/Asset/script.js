f999fe061c1136188f14c6f6bd87002dbf929272
Mikey J <mikeylashaun@icloud.com>
2022-04-27 19:17:34 -0700

    trying to figure out what part I am missing for the modal

--- Main/Asset/script.js
+++ Main/Asset/script.js
@@ -1,14 +1,17 @@
 // Create a variable that connects to the hero class in the header. Or that connects to the dateTime class
-let dateTime = $(".dateTime"); 
-setTime();
-console.log(dateTime); 
+let dateTime = $(".dateTime");
+
+
 // Set up a setinterval() fuction so you can update the text in the hero. 
-    // Every second update the format of moment with a new version of info
+// Every second update the format of moment with a new version of info
+
+
+
 
 
 function setTime() {
-    let clock = setInterval(function() {
-        dateTime.text(moment().format("MMMM Do YYYY , h:mm:ss a")) 
-    }, 1); 
+    let clock = setInterval(function () {
+        dateTime.text(moment().format("MMMM Do YYYY , h:mm:ss a"))
+    }, 0);
 };
-
+setTime();
