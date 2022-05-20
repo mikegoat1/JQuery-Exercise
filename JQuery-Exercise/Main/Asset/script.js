536b4bb093565868ac93dec9a2b0c6a4fa037fe1
Mikey J <mikeylashaun@icloud.com>
2022-05-20 10:02:13 -0700

    adding changes

---
+++ JQuery-Exercise/Main/Asset/script.js
@@ -0,0 +1,46 @@
+// Create a variable that connects to the hero class in the header. Or that connects to the dateTime class
+const dateTime = $(".dateTime");
+const modalSubmit = $("#submit"); 
+const modalForm1 = $("#modalForm"); 
+
+
+
+// Set up setInterval function to update text in hero. 
+// keep the update instantly
+function setTime() {
+    let clock = setInterval(function () {
+        dateTime.text(moment().format("MMMM Do YYYY , h:mm:ss a"))
+    }, 0);
+};
+setTime();
+
+// Function for widget of datepicker
+
+$("#date-picker").datepicker();
+
+// next thing you need to do is make sure you can grab the value out of these inputs
+
+
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const projectName = $("#project-name").val();
+    const projectType = $("#project-type").val();
+    const hourlyWage = $("#hourly-wage").val();
+    const datePicker = $("#date-picker").val(); 
+
+    // Adding function to remove items from input
+    $("select option[value='']").attr("selected", true); 
+    $("#modalForm input[type='text']").val('');
+    console.log(projectName); 
+    console.log(projectType); 
+    console.log(hourlyWage); 
+    console.log(datePicker); 
+
+    
+
+
+}
+console.log(modalSubmit)
+modalSubmit.on("click", handleFormSubmit); 
