aeed6a60ab23c07598ac51bed6dfaa5237128928
Mikey J <mikeylashaun@icloud.com>
2022-06-06 22:37:28 -0700

    working on fixing the correct amount of days between dates

--- Main/Asset/script.js
+++ Main/Asset/script.js
@@ -62,6 +62,7 @@ function handleFormSubmit(event) {
     // resetting input after recorded. 
     $("select option[value='']").attr("selected", true); 
     $("#modalForm input[type='text']").val('');
+
     //  create and add elements dynamically to the table. 
     //  Run function to calculate input. 
     //  When creating make sure to set to local storage
@@ -72,9 +73,15 @@ function handleFormSubmit(event) {
     console.log(projectType); 
     console.log(hourlyWage); 
     console.log(datePicker); 
+    console.log("Curren day:", moment("12-25-1995", "MM-D-YYYY").format("MM-DD-YYYY"))
+    // Experiment to get the diff function to give the correct number of days. 
+    let dayDifference =  moment(datePicker , "MM-D-YYYY").diff(moment(), "days"); 
+    dayDifference = 1 + dayDifference; 
+    console.log("Day difference:", dayDifference); 
+
+    console.log("Difference:" , moment(datePicker , "MM-D-YYYY").diff(moment(), "days"))
+
 
 }
-
-console.log(moment()); 
-
+console.log("Example:", moment("12-25-1995", "MM-DD-YYYY").format("MMM Do, YYYY"))
 modalSubmit.on("click", handleFormSubmit); 
